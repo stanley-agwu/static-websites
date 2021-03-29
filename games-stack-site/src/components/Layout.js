2ce464bc7c2b58604a3c3d92ea4976a579fdf1b9
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-29 17:32:53 +0200

    added globalStyles

--- games-stack-site/src/components/Layout.js
+++ games-stack-site/src/components/Layout.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Footer from "./Footer"
 import Navbar from "./Navbar"
+import "../styles/globalStyles.css"
 
 export default function Layout ({ children }){
     return (
