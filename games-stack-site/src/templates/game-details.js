5c760d3b43bc2bf08a14b701fc88228ee9cf22b4
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2021-03-31 19:01:13 +0200

    added changes

--- games-stack-site/src/templates/game-details.js
+++ games-stack-site/src/templates/game-details.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import Img from "gatsby-image"
+import * as styles from "../styles/gameDetails.module.css"
 
 
-export default function PortfolioDetails() {
+export default function gameDetails() {
     return (
         <Layout>
-            <div>
+            <div className={styles.details}>
                 <h2>title</h2>
                 <h3>stack</h3>
-                <div>
+                <div className={styles.featured}>
                     <Img fluid={} />
                 </div>
                 <div dangerouslySetInnerHTML={} />
